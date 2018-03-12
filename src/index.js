a805bb232e064d6c808e3b9afe0eb92d5a218890
unknown <v_green@hotmail.co.uk>
2018-03-12 16:44:40 +0000

    Save before ejecting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Docs from './docs/Docs';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
-registerServiceWorker();
